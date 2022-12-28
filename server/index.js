bb83cea28bae96c415b8803a5d1bd52a499a400b
Abissa <a.sani@alustudent.com>
2022-12-28 07:19:25 -0800

    feat: basic routing and data

--- server/index.js
+++ server/index.js
@@ -1,8 +1,33 @@
 const express = require("express");
+const logger = require("morgan");
+const tourRouter = require("./routes/tourRoutes");
+
 const PORT = 9000;
 
 const app = express();
+const cors = require("cors");
 
-app.listen(PORT, () => {
-    console.log(`App running at ${PORT}`); // eslint-disable-line
+app.use(cors());
+app.use(logger("dev"));
+
+// Middleware of request time
+var requestTime = function (req, res, next) {
+  req.requestTime = new Date().toISOString();
+  next();
+};
+
+app.use(requestTime);
+
+app.use("/api/v1/users", tourRouter);
+app.use("/api/v1/tours", tourRouter);
+
+// Middleware for 404 routes
+app.all("*", (req, res, next) => {
+  next(new Error(`Can’t find ${req.originalUrl} on this server`));
+});
+
+const server = app.listen(PORT, () => {
+  // const { address, port } = server.address();
+  console.log(`App running at http://127.0.0.1:${PORT}`); // eslint-disable-line
+  // console.log(server); // eslint-disable-line
 });
