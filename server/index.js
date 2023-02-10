e9719c0f3daa77057e69e2d1aea10aae276c7e53
Abissa <a.sani@alustudent.com>
2023-02-10 00:51:57 -0800

    feat: more auth - security

--- server/index.js
+++ server/index.js
@@ -1,44 +1,71 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const logger = require("morgan");
+const rateLimit = require("express-rate-limit");
+const helmet = require("helmet");
+const mongoSanitize = require("express-mongo-sanitize");
+const xss = require("xss-clean");
 const globalErrorHandler = require("./middlewares/error.handler");
 const AppError = require("./utils/app-error");
 const authRouter = require("./routes/auth.routes");
 const userRouter = require("./routes/user.routes");
 const tourRouter = require("./routes/tour.routes");
 
-require("dotenv").config();
+require("dotenv").config({ path: `.env.${process.env.NODE_ENV}` });
 
 process.on("uncaughtException", (err) => {
   console.error(err);
   process.exit(1);
 });
 
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  handler: () =>
+    new AppError(
+      "Too many requests from this client, please try again later",
+      429
+    ),
+});
+
 const PORT = process.env.PORT || 9000;
 
 const app = express();
+// parse body json data with limit of 10kb
+app.use(express.json({ limit: "10kb" }));
+// parse data from urlencoded form
+app.use(express.urlencoded({ extended: true, limit: "10kb" }));
+// set security HTTP headers
 
+// https://www.npmjs.com/package/helmet
+// https://expressjs.com/en/advanced/best-practice-security.html
+app.use(helmet());
 const DB = process.env.MONGO_URI;
 
 mongoose
   .connect(DB, {
     useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
   })
   .then(() => console.log("DB connection successful!")); // eslint-disable-line
 
 const cors = require("cors");
-
+app.use("/api", limiter);
 app.use(cors());
-app.use(express.json());
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+app.use(xss());
+
+// protect against parameter pollution
+// add the parameters you want to whitelist (to be duplicated in the query string)
+app.use(
+  hpp({
+    whitelist: [],
+  })
+);
 app.use(logger("dev"));
-
-// Middleware of request time
-var requestTime = function (req, res, next) {
-  req.requestTime = new Date().toISOString();
-  next();
-};
-
-app.use(requestTime);
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
@@ -49,7 +76,7 @@ app.all("*", (req, res, next) => {
   next(new AppError(`Can’t find ${req.originalUrl} on this server`));
 });
 
-// error handling middleware in express
+// error handling
 app.use(globalErrorHandler);
 
 const server = app.listen(PORT, () => {
